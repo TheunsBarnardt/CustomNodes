09d606415240bd5bab308915ea83f15316fa318f
TheunsBarnardt <theuns@barnardt.co.za>
2025-02-09 13:42:43 +0200

    First Commit

---
+++ n8n-nodes-dataverse-auth/credentials/dataverseAuth.credentials.ts
@@ -0,0 +1,98 @@
+import {
+	ICredentialType,
+	INodeProperties,
+} from 'n8n-workflow';
+
+// eslint-disable-next-line n8n-nodes-base/cred-class-name-unsuffixed
+export class dataverseAuth implements ICredentialType {
+	// eslint-disable-next-line n8n-nodes-base/cred-class-field-name-unsuffixed
+	name = 'dataverseAuth';
+	// eslint-disable-next-line n8n-nodes-base/cred-class-field-display-name-missing-api
+	displayName = 'Dataverse Auth';
+	properties: INodeProperties[] = [
+		{
+			displayName: 'Key Type',
+			name: 'keyType',
+			type: 'options',
+			options: [
+				{
+					name: 'Passphrase',
+					value: 'passphrase',
+				},
+				{
+					name: 'Private Key',
+					value: 'privateKey',
+				},
+				{
+					name: 'Public Key',
+					value: 'publicKey',
+				},
+			],
+			default: 'passphrase',
+		},
+		{
+			displayName: 'Private Key',
+			name: 'privateKey',
+			type: 'string',
+			typeOptions: {
+				password: true,
+			},
+			displayOptions: {
+				show: {
+					keyType: [
+						'privateKey',
+					],
+				},
+			},
+			default: '',
+		},
+		{
+			displayName: 'Public Key',
+			name: 'publicKey',
+			type: 'string',
+			typeOptions: {
+				password: true,
+			},
+			displayOptions: {
+				show: {
+					keyType: [
+						'publicKey',
+					],
+				},
+			},
+			default: '',
+		},
+		{
+			displayName: 'Passphrase',
+			name: 'passphrase',
+			type: 'string',
+			typeOptions: {
+				password: true,
+			},
+			displayOptions: {
+				show: {
+					keyType: [
+						'privateKey',
+					],
+				},
+			},
+			default: '',
+		},
+		{
+			displayName: 'Secret',
+			name: 'secret',
+			type: 'string',
+			typeOptions: {
+				password: true,
+			},
+			displayOptions: {
+				show: {
+					keyType: [
+						'passphrase',
+					],
+				},
+			},
+			default: '',
+		},
+	];
+}
